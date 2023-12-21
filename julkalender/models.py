8dc19ebc995f82d5b0ba5c2ccd71e78d8b720f96
Olle Mineur <olle.mineur@gmail.com>
2023-12-21 21:42:40 +0100

    saveWithTargetSize

--- julkalender/models.py
+++ julkalender/models.py
@@ -7,6 +7,13 @@ from django.dispatch import receiver
 
 from app.settings_shared import MEDIA_ROOT
 
+# JPGSaveWithTargetSize
+import io
+import math
+import sys
+import subprocess
+import shlex
+
 def update_filename(instance, filename):
     path = "doorcontent/"
     randomfolder = get_random_string(length=24)
@@ -30,9 +37,19 @@ class Door(models.Model):
 
 @receiver(post_save, sender=Door)
 def deleting_old_image(sender, instance, **kwargs):
+    saveWithTargetSize(instance.image.path, 1)
+
     for root, dirs, files in os.walk(os.path.join(MEDIA_ROOT, "doorcontent")):
         for d in dirs:
             dir = os.path.join(root, d)
             # check if dir is empty
             if not os.listdir(dir):
                 os.rmdir(dir)
+
+def saveWithTargetSize(filename, target):
+    print(os.path.getsize(filename) / (1024 * 1024))
+    if os.path.getsize(filename) / (1024 * 1024) > target:
+        print("Resizing " + str(filename))
+        cmd = ["convert " + str(filename), "-define jpeg:extent=" + str(target) + "mb " + str(filename)]
+        print(" ".join(cmd))
+        subprocess.Popen(cmd, shell=True)
