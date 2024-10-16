0dd569caa869aafe68f6c28eb7d1ba9229bfd507
Olle Mineur <olle.mineur@gmail.com>
2024-10-16 20:24:35 +0200

    Changed back database

--- app/settings_production.py
+++ app/settings_production.py
@@ -8,14 +8,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.d-sektionen.se', '.dsektionen.se', '.datateknologsektionen.se']
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('POSTGRES_NAME', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django'),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
-    }
+    'default': dj_database_url.config(conn_max_age=500)
 }
 
 SECURE_CONTENT_TYPE_NOSNIFF = True
