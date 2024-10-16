8e548bbc4a32d1d9b57880a212664165462b8099
Olle Mineur <olle.mineur@gmail.com>
2024-10-16 20:37:51 +0200

    Remade some config for running in our docker setup

--- app/settings_production.py
+++ app/settings_production.py
@@ -8,7 +8,14 @@ DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.d-sektionen.se', '.dsektionen.se', '.datateknologsektionen.se']
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=500)
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_NAME", "django"),
+        "USER": os.getenv("POSTGRES_USER", "django"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "django"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    }
 }
 
 SECURE_CONTENT_TYPE_NOSNIFF = True
